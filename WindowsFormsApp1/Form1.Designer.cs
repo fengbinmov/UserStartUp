0a7dd6d9fd4db4cc825f45493cba02660b74ec5e
MOVCat <fengbinmov@outlook.com>
2020-03-09 15:43:00 +0800

    用户启动器v1.0

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace UserStartUp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "启动器";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 210);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "  ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace UserStartUp
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<int, string> dict = new Dictionary<int, string>();
+
+        void UpdateCom(App app) {
+
+            for (int i = 0; i < app.sub.Count; i++)
+            {
+                ToolStripMenuItem temp = new ToolStripMenuItem();
+
+                temp.Name = app.sub[i].name+"-"+ app.sub[i].id;
+                temp.Size = new Size(180, 22);
+                temp.Text = app.sub[i].name;
+                temp.Tag = app.sub[i].id.ToString();
+                if(!app.sub[i].path.Equals(String.Empty)) temp.Click += new EventHandler(OpenProgram);
+                app.sub[i].toolStripMenuItem = temp;
+
+                if (app.sub[i].level == 0)
+                {
+                    contextMenuStrip1.Items.Add(temp);
+                }
+                else {
+                    app.sub[i].up.toolStripMenuItem.DropDownItems.Add(temp);
+                }
+
+                if (app.sub[i].sub.Count > 0)
+                    UpdateCom(app.sub[i]);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            UpdateCom(LoadData());
+
+            GC.Collect();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void OpenProgram(object sender, EventArgs e) {
+
+            int id = int.Parse(((ToolStripMenuItem)sender).Tag.ToString());
+
+            Process.Start(dict[id]);
+        }
+
+        private App LoadData() {
+
+            App root;
+
+            string[] text = File.ReadAllLines(Application.StartupPath + "\\info.txt");
+
+            App nowUpApp = new App();
+            root = nowUpApp;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (String.IsNullOrWhiteSpace(text[i]) || text[i][0].Equals('#'))
+                    continue;
+
+                string line = text[i];
+                string[] split = line.Split(',');
+
+                string name = split[0];
+                string path = "";
+                for (int n = 1; n < split.Length; n++)
+                {
+                    path += split[n];
+                    if (n != split.Length - 1) path += ",";
+                }
+
+                int nowLevel = 0;
+                int charIndex = 0;
+                while (line[charIndex++].Equals('\t'))
+                {
+                    nowLevel++;
+                }
+                App ap = new App();
+                ap.name = name;
+                ap.path = path.Replace("\t", "");
+                ap.level = nowLevel;
+                ap.id = dict.Count;
+
+                if (nowUpApp.level != nowLevel - 1)
+                {
+                    if (nowLevel == nowUpApp.level)
+                    {
+                        nowUpApp = nowUpApp.up;
+                    }
+                    else if (nowLevel < nowUpApp.level)
+                    {
+                        while (nowUpApp.level != nowLevel - 1)
+                        {
+                            nowUpApp = nowUpApp.up;
+                        }
+                    }
+                    else {
+                        nowUpApp = nowUpApp.sub[nowUpApp.sub.Count - 1];
+                        ////格式出错
+                        //Application.Exit();
+                    }
+                }
+
+                ap.up = nowUpApp;
+                nowUpApp.sub.Add(ap);
+                dict.Add(ap.id, ap.path);
+            }
+
+            return root;
+        }
+
+    }
+
+    public class App{
+        public int level = -1;
+        public string name;
+        public string path;
+        public List<App> sub = new List<App>();
+        public App up;
+
+        public int id;
+        public ToolStripMenuItem toolStripMenuItem;
+    }
+}
